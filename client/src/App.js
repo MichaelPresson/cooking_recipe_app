8cacf6c223a23d82bbfcf6b465731a1d7ca495be
mason Passinault <passinaultm@gmail.com>
2024-06-07 20:16:00 -0400

    formating fixed for form

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header';
-import Form from './components/Form'
+import Form from './components/Form';
+import Recomend from './components/Recomend';
 
 function App() {
   return (
     <div className="App">
       <Header title="Grocery_List_App" />
       <Form/>
+      <Recomend/>
     </div>
   );
 }
---
+++ client/src/components/Recomend.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Recomend = () => {
+    return (
+        <div className='recomend-container'></div>
+    )
+};
+
+export default Recomend;
